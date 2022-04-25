a78650954da8e4f6bab5d64541373f16727ced8e
DayoungChoii <young12430@gmail.com>
2022-04-25 22:41:25 +0900

    Initial commit

---
+++ src/main/java/com/dayoung/ginseng/GinsengApplication.java
@@ -0,0 +1,13 @@
+package com.dayoung.ginseng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GinsengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GinsengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dayoung/ginseng/GinsengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dayoung.ginseng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GinsengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
